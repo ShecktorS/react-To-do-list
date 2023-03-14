1a346fe5685f02aa1a55145622c3adecbef6d449
ShecktorS <ettoresanfilippo@gmail.com>
2023-03-14 16:54:48 +0100

    Added functionality of task removal and completion

--- src/components/toDoItem/ToDoItem.jsx
+++ src/components/toDoItem/ToDoItem.jsx
@@ -1,11 +1,33 @@
 import "./index.css";
 
 const ToDoItem = ({ todo, setList }) => {
-  console.log(todo);
+  const changeToDoStatus = () => {
+    setList((prev) => [
+      ...prev.filter((item) => item.id != todo.id),
+      {
+        id: todo.id,
+        content: todo.content,
+        isCompleted: !todo.isCompleted,
+      },
+    ]);
+  };
+
+  const deleteTodo = () => {
+    setList((prev) => [...prev.filter((item) => item.id != todo.id)]);
+  };
   return (
     <li className="ToDoItem">
-      <p>{todo.isCompleted ? "✅" : "◻️"}</p>
-      <p>{todo.content}</p>
+      <p className="box" onClick={changeToDoStatus}>
+        {todo.isCompleted ? "✅" : "◻️"}
+      </p>
+      <p className={`content ${todo.isCompleted && "deleted"}`}>
+        {todo.content}
+      </p>
+      {todo.isCompleted && (
+        <p className="deleteTodo" onClick={deleteTodo}>
+          🗑️
+        </p>
+      )}
     </li>
   );
 };
