5b72f44ceddc4fc816ff41a142a25ed163171174
ShecktorS <ettoresanfilippo@gmail.com>
2023-03-14 17:15:16 +0100

    added warning message in ToDoList

--- src/components/toDoList/ToDoList.jsx
+++ src/components/toDoList/ToDoList.jsx
@@ -1,13 +1,15 @@
 import "./index.css";
-import toDoList from "../../mock/toDoList";
 import ToDoItem from "../toDoItem";
 
 const ToDoList = ({ list, setList }) => {
   return (
     <ul className="ToDoList">
-      {list.map((todo, i) => (
-        <ToDoItem todo={todo} key={i} setList={setList} />
-      ))}
+      {list
+        .sort((a, b) => a.id - b.id)
+        .map((todo, i) => (
+          <ToDoItem todo={todo} key={i} setList={setList} />
+        ))}
+      {list.length < 1 && <p>Nella lista non è presente alcun elemento!</p>}
     </ul>
   );
 };
